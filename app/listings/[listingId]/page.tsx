12b7a1edf0f1f99376d7337de4ce4923be3c30fe
HakSooKim <haksoo@glassdomeinc.com>
2023-06-28 00:01:36 +0900

    feat: Add reservation functionality

--- app/listings/[listingId]/page.tsx
+++ app/listings/[listingId]/page.tsx
@@ -2,6 +2,7 @@ import getCurrentUser from '@/app/actions/getCurrentUser';
 import getListingById from '@/app/actions/getListingById';
 import EmptyState from '@/app/components/EmptyState';
 import ListingClient from './ListingClient';
+import getReservations from '@/app/actions/getReservations';
 
 interface IParams {
   listingId?: string;
@@ -9,12 +10,13 @@ interface IParams {
 
 const ListingPage = async ({ params }: { params: IParams }) => {
   const listing = await getListingById(params);
+  const reservations = await getReservations(params);
   const currentUser = await getCurrentUser();
 
   if (!listing) {
     return <EmptyState />;
   }
-  return <ListingClient listing={listing} currentUser={currentUser} />;
+  return <ListingClient listing={listing} reservations={reservations} currentUser={currentUser} />;
 };
 
 export default ListingPage;
--- app/types/index.ts
+++ app/types/index.ts
@@ -1,6 +1,13 @@
-import { Listing, User } from '@prisma/client';
+import { Listing, Reservation, User } from '@prisma/client';
 
 export type SafeListing = Omit<Listing, 'createdAt'> & { createdAt: string };
+
+export type SafeReservation = Omit<Reservation, 'createdAt' | 'startDate' | 'endDate' | 'listing'> & {
+  createdAt: string;
+  startDate: string;
+  endDate: string | null;
+  listing: SafeListing;
+};
 
 export type SafeUser = Omit<User, 'createdAt' | 'updatedAt' | 'emailVerified'> & {
   createdAt: string;
